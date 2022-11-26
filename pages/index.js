348eb93b9a6cd86937a22c0196656acae1715246
Maciejka1 <maciekglab0812@vp.pl>
2022-11-26 12:53:56 +0100

    adding tasks now works

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,40 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react'
 export default function Home() {
-  let [todo, addTodo] = useState(['todoList', 'dlsadk'])
+  const [todo, addTodo] = useState([])
+  const [input, setInput] = useState('')
+  const [error, setError] = useState('')
   return (
     <div className='flex justify-center items-center w-screen h-screen'>
       <div className=''>
         <h1 className='text-3xl font-bold'>TODO</h1>
         <div className='flex gap-2'>
-          <input type="text" className='bg-gray-800 p-2 focus:bg-gray-900 rounded-full'/>
-          <div className='bg-gray-800 hover:bg-gray-900 p-2 rounded-full cursor-pointer'>Add</div>
+          <input type="text" className='bg-gray-800 p-2 focus:bg-gray-900 rounded-full'
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+          />
+          <div
+          className='bg-gray-800 hover:bg-gray-900 p-2 rounded-full cursor-pointer'
+          onClick={() => {
+            if (input){
+              let editTodo = todo
+              editTodo.unshift(input)
+              setError('') 
+              setInput('')
+            } else {
+              setError('You cannot add empty entry!')
+            }
+
+          }}
+          >
+            Add
+          </div>
         </div>
+        <p className='text-red-600'>{error}</p>
         <div className='flex flex-col'>
           {todo.map((task) => {
             return(
-              <p>{task}</p>
+              <p key={task + task.key}>{task}</p>
             )
           })}
         </div>
